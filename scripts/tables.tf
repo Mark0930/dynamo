2661aa40858b06d75f45e0526f21eaac9f96ca35
Mark0930 <Mark814690@gmail.com>
2018-11-06 20:30:08 +0000

    added dynamo table and item

---
+++ scripts/tables.tf
@@ -0,0 +1,23 @@
+resource "aws_dynamodb_table" "example" {
+  name           = "CoffeeShops"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "name"
+
+  attribute {
+    name = "name"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table_item" "basic" {
+  table_name = "${aws_dynamodb_table.example.name}"
+  hash_key = "${aws_dynamodb_table.example.hash_key}"
+  item = <<ITEM
+{
+  "name": {"S": "Pollen"},
+  "address": {"S": "new islington"},
+  "rating": {"N": "9"}
+}
+ITEM
+}
